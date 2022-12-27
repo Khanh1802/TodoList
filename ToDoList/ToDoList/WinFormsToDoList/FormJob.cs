30f0e387362da66303092195cc74f9d92177f64b
37688 <cuong012qn121@gmail.com>
2022-12-27 19:36:35 +0700

    fix bug

--- ToDoList/ToDoList/WinFormsToDoList/FormJob.cs
+++ ToDoList/ToDoList/WinFormsToDoList/FormJob.cs
@@ -1,25 +1,19 @@
-﻿using System.Xml.Linq;
-using ToDoList.Data.Models;
-using static System.Windows.Forms.AxHost;
-using ToDoList.Services;
+﻿using ToDoList.Data.Models;
 using ToDoList.Dtos.Jobs;
+using ToDoList.Services;
 
 namespace WinFormsToDoList
 {
     public partial class FormJob : Form
     {
         private readonly IJobService _jobService;
-        private readonly ICategoryService _categoryService;
-        private readonly IStateService _stateService;
-        private Job _job;
-        private JobDto _jobDto;
         private bool _loadingDone = false;
-        public FormJob(IJobService jobService, ICategoryService categoryService, IStateService stateService)
+        private int? selectJobId = null;
+
+        public FormJob(IJobService jobService)
         {
             InitializeComponent();
             _jobService = jobService;
-            _categoryService = categoryService;
-            _stateService = stateService;
         }
 
         private async void BtAdd_Click(object sender, EventArgs e)
@@ -80,14 +74,12 @@ namespace WinFormsToDoList
         {
             if (_loadingDone)
             {
-                if (_jobDto is not null)
+                if (selectJobId is not null)
                 {
                     if (!string.IsNullOrEmpty(TbName.Text))
                     {
                         try
                         {
-                            _jobDto.NameJob = TbName.Text;
-                            _jobDto.LastModificationTime = DateTime.Now;
                             //await _stateService.UpdateAsync(_state);
                             await _jobService.UpdateAsync(new UpdateJobDto()
                             {
@@ -97,7 +89,7 @@ namespace WinFormsToDoList
                                 FromDate = DTPFrom.Value,
                                 ToDate = DTPTo.Value,
                                 Name = TbName.Text,
-                                Id = _jobDto.Id,
+                                Id = selectJobId!.Value
                             });
                             MessageBox.Show("Change state completed", "Done", MessageBoxButtons.OK);
 
@@ -119,13 +111,13 @@ namespace WinFormsToDoList
         {
             if (_loadingDone)
             {
-                if (_jobDto is not null)
+                if (selectJobId is not null)
                 {
                     DialogResult dialogResult = MessageBox.Show("Are you sure want to delete this job", "Delete record", MessageBoxButtons.YesNo);
                     if (dialogResult == DialogResult.Yes)
                     {
                         TbName.Text = string.Empty;
-                        await _jobService.DeleteAsync(_jobDto.Id);
+                        await _jobService.DeleteAsync(selectJobId);
                         await RefreshDataGridView();
                         MessageBox.Show("Record has been successfully deleted", "Done", MessageBoxButtons.OK);
                     }
@@ -154,7 +146,7 @@ namespace WinFormsToDoList
             BtRemove.Enabled = false;
             BtUpdate.Enabled = false;
             BtAdd.Enabled = true;
-            _job = null;
+            selectJobId = null;
             //var listCategory = await _categoryService.GetAllAsync();
             //var resultCategory = listCategory.Where(x => x.IsDeleted != true).ToList();
             //CbbCategory.DataSource = resultCategory;
@@ -174,20 +166,20 @@ namespace WinFormsToDoList
                 BtRemove.Enabled = false;
                 BtUpdate.Enabled = false;
                 BtAdd.Enabled = true;
-                _job = null;
-                return;
+                selectJobId = null;
             }
             else
             {
                 // Dtg.Rows[e.RowIndex] lấy hàng đang CellClick hiện tại
                 var row = Dtg.Rows[e.RowIndex];
-                // row.Cells[0].Value.ToString() lấy id rồi convert
-                int id = Convert.ToInt32(row.Cells[0].Value.ToString());
-                _jobDto = await _jobService.GetByIdAsync(id);
-                TbName.Text = _jobDto.NameJob;
-                BtAdd.Enabled = false;
-                BtRemove.Enabled = true;
-                BtUpdate.Enabled = true;
+                if (Dtg.Rows[e.RowIndex].DataBoundItem is JobDto jobDto)
+                {
+                    selectJobId = jobDto.Id;
+                    TbName.Text = jobDto.NameJob;
+                    BtAdd.Enabled = false;
+                    BtRemove.Enabled = true;
+                    BtUpdate.Enabled = true;
+                }
             }
         }
     }
