02d786da28d24a87b8c88520d9d420f6ac9e9689
Khanh1802 <Khanh1802@github.com>
2022-12-29 13:18:23 +0700

    Change name StateForm

--- ToDoList/ToDoList/WinFormsToDoList/FormState.Designer.cs
+++ ToDoList/ToDoList/WinFormsToDoList/FormState.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.Dtg = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.BtRemove = new System.Windows.Forms.Button();
             this.BtUpdate = new System.Windows.Forms.Button();
             this.BtAdd = new System.Windows.Forms.Button();
@@ -48,7 +47,7 @@
             // 
             this.Dtg.BackgroundColor = System.Drawing.Color.White;
             this.Dtg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dtg.Location = new System.Drawing.Point(116, 200);
+            this.Dtg.Location = new System.Drawing.Point(116, 204);
             this.Dtg.Name = "Dtg";
             this.Dtg.ReadOnly = true;
             this.Dtg.RowHeadersWidth = 51;
@@ -58,16 +57,6 @@
             this.Dtg.TabIndex = 41;
             this.Dtg.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dtg_CellClick);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(275, -41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 38);
-            this.label1.TabIndex = 36;
-            this.label1.Text = "CREATE CATEGORY";
-            // 
             // BtRemove
             // 
             this.BtRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
@@ -75,7 +64,7 @@
             this.BtRemove.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.BtRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtRemove.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtRemove.Location = new System.Drawing.Point(746, 522);
+            this.BtRemove.Location = new System.Drawing.Point(746, 526);
             this.BtRemove.Name = "BtRemove";
             this.BtRemove.Size = new System.Drawing.Size(123, 40);
             this.BtRemove.TabIndex = 40;
@@ -90,7 +79,7 @@
             this.BtUpdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.BtUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtUpdate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtUpdate.Location = new System.Drawing.Point(449, 520);
+            this.BtUpdate.Location = new System.Drawing.Point(449, 524);
             this.BtUpdate.Name = "BtUpdate";
             this.BtUpdate.Size = new System.Drawing.Size(123, 40);
             this.BtUpdate.TabIndex = 39;
@@ -105,7 +94,7 @@
             this.BtAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtAdd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.BtAdd.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.BtAdd.Location = new System.Drawing.Point(114, 520);
+            this.BtAdd.Location = new System.Drawing.Point(114, 524);
             this.BtAdd.Name = "BtAdd";
             this.BtAdd.Size = new System.Drawing.Size(123, 40);
             this.BtAdd.TabIndex = 38;
@@ -118,7 +107,7 @@
             this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.TbName);
-            this.panel1.Location = new System.Drawing.Point(221, 120);
+            this.panel1.Location = new System.Drawing.Point(221, 124);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(545, 66);
             this.panel1.TabIndex = 37;
@@ -127,7 +116,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(16, 16);
+            this.label4.Location = new System.Drawing.Point(16, 20);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 28);
             this.label4.TabIndex = 13;
@@ -135,7 +124,7 @@
             // 
             // TbName
             // 
-            this.TbName.Location = new System.Drawing.Point(188, 16);
+            this.TbName.Location = new System.Drawing.Point(188, 20);
             this.TbName.Name = "TbName";
             this.TbName.Size = new System.Drawing.Size(290, 27);
             this.TbName.TabIndex = 10;
@@ -144,22 +133,22 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(368, 22);
+            this.label2.Location = new System.Drawing.Point(398, 26);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(251, 38);
+            this.label2.Size = new System.Drawing.Size(191, 38);
             this.label2.TabIndex = 42;
-            this.label2.Text = "CREATE CATEGORY";
+            this.label2.Text = "CREATE STATE";
             // 
             // TbFind
             // 
-            this.TbFind.Location = new System.Drawing.Point(221, 74);
+            this.TbFind.Location = new System.Drawing.Point(221, 78);
             this.TbFind.Name = "TbFind";
             this.TbFind.Size = new System.Drawing.Size(545, 27);
             this.TbFind.TabIndex = 43;
             // 
             // BtFind
             // 
-            this.BtFind.Location = new System.Drawing.Point(806, 74);
+            this.BtFind.Location = new System.Drawing.Point(806, 78);
             this.BtFind.Name = "BtFind";
             this.BtFind.Size = new System.Drawing.Size(94, 27);
             this.BtFind.TabIndex = 44;
@@ -171,7 +160,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(116, 74);
+            this.label3.Location = new System.Drawing.Point(116, 78);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(70, 28);
             this.label3.TabIndex = 45;
@@ -187,7 +176,6 @@
             this.Controls.Add(this.TbFind);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Dtg);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.BtRemove);
             this.Controls.Add(this.BtUpdate);
             this.Controls.Add(this.BtAdd);
@@ -207,7 +195,6 @@
         #endregion
 
         private DataGridView Dtg;
-        private Label label1;
         private Button BtRemove;
         private Button BtUpdate;
         private Button BtAdd;
